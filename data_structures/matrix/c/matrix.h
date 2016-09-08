9555382b3601125c093e52759f6379f3d5fe8590
Luis Manuel Averhoff <laver008@fiu.edu>
2016-09-07 22:33:22 -0400

    Defined generic matrix operations

--- data_structures/matrix/c/matrix.h
+++ data_structures/matrix/c/matrix.h
@@ -1,23 +1,17 @@
-#include <stdio.h> // for input and output (printf)
-#include <string.h> //for assignment of strings and names
-#include <stdlib.h> //for atoi(char*) function 
-#include <limits.h> //for int min/max
+#ifndef Matrix_H_INCLUDED
+#define Matrix_H_INCLUDED
 
+typedef struct Matrix
+{
+    void **data;
+    size_t rows;
+    size_t columns;
+    size_t memSize;
+}matrix;
 
+int creatematrix(matrix *, size_t, size_t, size_t);
+void *getElement(matrix *, size_t, size_t);
+void setElement(matrix *, size_t, size_t, void *);
+void freematrix(matrix *);
 
-/*Typedef declaration for integer matrix*/
-
-struct imatrix {
-char* name ; 
-int rows ; 
-int columns ;
-int list[100][100] ;
-
-};
-
-typedef struct imatrix iMatrix ;
-
-
-iMatrix createIntMatrix(int **, const int, const int, char*);
-
-void printComponents(iMatrix);
+#endif /* Matrix_H_INCLUDED */
