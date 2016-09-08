8c183138cf2995225c7453236f74781cb40bba0e
Luis Manuel Averhoff <laver008@fiu.edu>
2016-09-08 02:29:32 -0400

    Update matrixtest.c

--- data_structures/matrix/c/matrixtest.c
+++ data_structures/matrix/c/matrixtest.c
@@ -5,11 +5,19 @@
 int main(int argc, char* argv[])
 {
     matrix mat;
-    int i, j, element; /*Change element to the appropriate data type you will be workng with.*/
+    int i, j, errCheck, element; /*Change element to the appropriate data type you will be workng with.*/
     const int ROWS = atoi(argv[1]);
     const int COL = atoi(argv[2]);
 
-    creatematrix(&mat, ROWS, COL, sizeof(int)); /*You can change sizeof() for any data type you want(integers, char arrays.)*/
+    /*You can change sizeof() for any data type you want(integers, char arrays.)*/
+    errCheck = creatematrix(&mat, ROWS, COL, sizeof(int));
+    
+    /*If we were not able to successfully allocate memory*/
+    if(errCheck == -1)
+    {
+        printf("Couldn't not allocate any more memory.");
+        exit(EXIT_FAILURE);
+    }
 
     for(i = 0; i < ROWS; i++)
     {
