95aad14dd7fc99313f3c30333d84891b257a51d5
Luis Manuel Averhoff <laver008@fiu.edu>
2016-09-09 00:27:40 -0400

    Update PrimeChecker.c

--- algorithms/PrimeChecker/PrimeChecker.c
+++ algorithms/PrimeChecker/PrimeChecker.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <math.h>
 
-int isPrime(int);
+int isPrime(long);
 
 int main()
 {
-    int num;
+    long num;
 
     printf("Enter a number and I'll check if it is a prime number. ");
-    scanf(" %d", &num);
+    scanf(" %ld", &num);
 
     printf("\n");
 
@@ -27,9 +27,19 @@ int main()
     return 0;
 }
 
-int isPrime(int num)
+int isPrime(long num)
 {
     int i;
+    float sqrt_limiter = sqrt(num);
+
+    if(num == 2 || num == 3)
+    {
+        return 1;
+    }
+    else if(num % 2 == 0 || num % 3 == 0)
+    {
+        return 0;
+    }
 
     /*
       We only have to go up to the square root of n because once we past the square root of n,
@@ -39,10 +49,16 @@ int isPrime(int num)
       Take 36 for example, our cofactors are {1, 36}, {2, 18}, {3, 12}, {4. 9}, {6, 6}, {9, 4}, {12, 3}, {18, 2}, {36, 1}.
       Notice how after {6, 6} all of our factors started to repeat. This is why we don't go past the square of
       n(or 6 in this case).
+
+      Here we are doing an important optimization to make a trial division a little bit more efficient
+      such as checking if the number is divisible by 2 or 3. This check eliminates the need to check numbers
+      that have the number 2 or 3 as its prime factor. This means that only prime factors that we have to look
+      out for are 5 and 7. This also means that we can simply start at 6 and add plus and minus one to check if the
+      current number that we are checking is a prime factor of 5 and 7.
     */
-    for(i = 2; i < sqrt(num); i++)
+    for(i = 6; i < sqrt_limiter; i+=6)
     {
-       if((num % i) == 0)
+       if((num % (i - 1) == 0) || (num % (i + 1) == 0))
        {
            return 0;
        }
