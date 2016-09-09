5fc1a033903d6d54e976cd3599d23f8fb3176fe3
Luis Manuel Averhoff <laver008@fiu.edu>
2016-09-09 00:32:50 -0400

    Update PrimeChecker.c

--- algorithms/PrimeChecker/PrimeChecker.c
+++ algorithms/PrimeChecker/PrimeChecker.c
@@ -42,13 +42,11 @@ int isPrime(long num)
     }
 
     /*
-      We only have to go up to the square root of n because once we past the square root of n,
-      all the cofactors of n will repeat. That is why it is efficient to go up to square root of n
-      because if we have not found an a and b, where a and b are other positive numbers besides 1 and n,
-      then by the time we reach square root of n, it is guaranteed that we will never find a and b.
-      Take 36 for example, our cofactors are {1, 36}, {2, 18}, {3, 12}, {4. 9}, {6, 6}, {9, 4}, {12, 3}, {18, 2}, {36, 1}.
-      Notice how after {6, 6} all of our factors started to repeat. This is why we don't go past the square of
-      n(or 6 in this case).
+      The reason why it is efficient to go up to the square root of n is because if there isn't an a and b, such that a and b 
+      are positive numbers besides 1 and n, then by the time we reach square root of n, it is guaranteed that we 
+      will never find an a and b that is not 1 and n. Take 36 for example, our cofactors are {1, 36}, {2, 18}, {3, 12}, {4. 9}, {6, 6}, {9, 4}, 
+      {12, 3}, {18, 2}, {36, 1}. Notice how after {6, 6} all of our factors started to repeat. This is why we don't go past 
+      the square of n(or 6 in this case).
 
       Here we are doing an important optimization to make a trial division a little bit more efficient
       such as checking if the number is divisible by 2 or 3. This check eliminates the need to check numbers
