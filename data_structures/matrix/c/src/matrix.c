1bde3533bf227a164490451fd783fd318cf6843a
Luis Manuel Averhoff <laver008@fiu.edu>
2016-09-12 00:16:04 -0400

    Update matrix.c

--- data_structures/matrix/c/src/matrix.c
+++ data_structures/matrix/c/src/matrix.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 #include "Matrix.h"
 
 int createMatrix(matrix *mat, size_t rows, size_t columns, size_t memSize)
 {
-    assert(mat); /* Should not be null at this point. */
-    assert(!mat->data); /* Should not have any more allocated to it at this point. */
     void **data = calloc(rows * columns, memSize);
 
     if(data != NULL) /*In case calloc fails*/
@@ -26,23 +23,26 @@ void setElement(matrix *mat, size_t x, size_t y, void *data)
 {
     /*
        Remember this formula. (i * numOfColumns) + j to traverse a 2D array in a 1D fashion(This formula only applies to Row Major Order Arrays).
-       We are also multiplying by the appropriate memory size because different types of data have different memory sizes.
     */
-    size_t offset = (x * mat->columns * mat->memSize) + (y * mat->memSize);
+    size_t offset = (x * mat->columns) + y;
     memcpy(mat->data + offset, data, mat->memSize);
 }
 
 void *getElement(matrix *mat, size_t x, size_t y)
 {
-    size_t offset = (x * mat->columns * mat->memSize) + (y * mat->memSize);
+    size_t offset = (x * mat->columns)  + y ;
     return mat->data + offset;
 }
 
 void freeMatrix(matrix *mat)
 {
-    if(mat != NULL)
+    if(mat->data != NULL)
     {
+       /*Set back every back to their default values to preserve invariants.*/
         free(mat->data);
-        *mat = {NULL, 0, 0, mat->memSize};
+        mat->data = NULL;
+        mat->rows = 0;
+        mat->columns = 0;
+        mat->memSize = 0;
     }
 }
