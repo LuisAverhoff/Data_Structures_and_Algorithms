82503f6e301789ca6b155807e5a9b5ffbc07ea8e
Luis Manuel Averhoff <laver008@fiu.edu>
2016-09-12 01:53:11 -0400

    Update matrix.c

--- data_structures/matrix/c/src/matrix.c
+++ data_structures/matrix/c/src/matrix.c
@@ -3,13 +3,8 @@
 #include <string.h>
 #include "Matrix.h"
 
-int createMatrix(matrix *mat, size_t rows, size_t columns, size_t memSize)
+int createMatrix(matrix *mat, const size_t rows, const size_t columns, const size_t memSize)
 {
-    if(rows < 0 || columns < 0)
-    {
-        return -1;
-    }
-    
     void **data = calloc(rows * columns, memSize);
 
     if(data != NULL) /*In case calloc fails*/
@@ -24,19 +19,18 @@ int createMatrix(matrix *mat, size_t rows, size_t columns, size_t memSize)
     return -1;
 }
 
-void setElement(matrix *mat, size_t x, size_t y, void *data)
+void setElement(matrix *mat, const size_t x, const size_t y, const void *data)
 {
     /*
        Remember this formula. (i * numOfColumns) + j to traverse a 2D array in a 1D fashion(This formula only applies to Row Major Order Arrays).
-       We are also multiplying by the appropriate memory size because different types of data have different memory sizes.
     */
-    size_t offset = (x * mat->columns * mat->memSize) + (y * mat->memSize);
+    size_t offset = (x * mat->columns) + y;
     memcpy(mat->data + offset, data, mat->memSize);
 }
 
-void *getElement(matrix *mat, size_t x, size_t y)
+void *getElement(matrix *mat, const size_t x, const size_t y)
 {
-    size_t offset = (x * mat->columns * mat->memSize) + (y * mat->memSize);
+    size_t offset = (x * mat->columns)  + y ;
     return mat->data + offset;
 }
 
