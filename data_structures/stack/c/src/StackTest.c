14f0b06404690a3d71587b45b7f477450775e011
Luis Manuel Averhoff <laver008@fiu.edu>
2016-09-12 02:38:28 -0400

    Update and rename data_structures/stack/c/StackLinkedList.c to data_structures/stack/c/src/StackTest.c

---
+++ data_structures/stack/c/src/StackTest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+
+int main()
+{
+    int val;
+    Stack s;
+
+    createStack(&s, sizeof(int));
+
+    for(val = 1; val <= 10; val++)
+    {
+        StackPush(&s, &val);
+    }
+
+    while(!isStackEmpty(&s))
+    {
+        StackPop(&s, &val);
+        printf("%d has been popped off the stack.\n", val);
+    }
+
+    freeStack(&s);
+
+    return 0;
+}
