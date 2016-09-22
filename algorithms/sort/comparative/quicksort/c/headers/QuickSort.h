c71a73dfc8ac7ffc48e0513aa3b6e7f7cada0d79
Luis Manuel Averhoff <laver008@fiu.edu>
2016-09-22 00:09:11 -0400

    Update QuickSort.h

--- algorithms/sort/comparative/quicksort/c/headers/QuickSort.h
+++ algorithms/sort/comparative/quicksort/c/headers/QuickSort.h
@@ -3,9 +3,9 @@
 
 /*
   This threshold signifies when we should stop quick sort and call insertion sort. Say for example we try to sort 10000 items, by the time
-  we have a sub array of 20 elements, the array is short and sorted enough that insertion sort would perform better than quick sort.
+  we have a sub array of 10 elements, the array is short and sorted enough that insertion sort would perform better than quick sort.
 */
-#define THRESHOLD 20
+#define THRESHOLD 10
 
 void quickSort_h(void *, size_t, size_t, int (*cmp)(const void *, const void *));
 void quickSort(void *, int, int, size_t, int (*cmp)(const void *, const void *));
