d65ca60c5719679355180bf8fe45c30e8c2b73e4
Luis Manuel Averhoff <laver008@fiu.edu>
2016-09-23 09:48:25 -0400

    Update HeapSort.h

--- algorithms/sort/comparative/heapsort/c/headers/HeapSort.h
+++ algorithms/sort/comparative/heapsort/c/headers/HeapSort.h
@@ -1,7 +1,9 @@
 #ifndef HEAPSORT_H_INCLUDED
 #define HEAPSORT_H_INCLUDED
 
-void heapSort(void *, size_t, size_t, int (*cmp)(const void *, const void *));
+typedef int (*cmp_t)(const void *, const void *);
+
+void heapSort(void *, size_t, size_t, cmp_t);
 void byteSwap(void *, void *, size_t);
 
 #endif
