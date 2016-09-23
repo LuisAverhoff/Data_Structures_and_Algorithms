407fc9172736c27dbc15700e9ee964cd31565884
Luis Manuel Averhoff <laver008@fiu.edu>
2016-09-23 01:29:36 -0400

    Update HeapSort.h

--- algorithms/sort/comparative/heapsort/c/headers/HeapSort.h
+++ algorithms/sort/comparative/heapsort/c/headers/HeapSort.h
@@ -2,11 +2,6 @@
 #define HEAPSORT_H_INCLUDED
 
 void heapSort(void *, size_t, size_t, int (*cmp)(const void *, const void *));
-void buildHeap(void *, size_t, size_t, int (*cmp)(const void *, const void *));
-void siftDown(void *, int, int, size_t, int (*cmp)(const void *, const void *));
-int childSearch(void *, int, int, size_t, int (*cmp)(const void *, const void *));
-int calParentIndex(int);
-int calLeftChildIndex(int);
 void byteSwap(void *, void *, size_t);
 
 #endif
