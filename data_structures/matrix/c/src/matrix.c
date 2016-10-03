a79ce7e6bafa905a1bef446172a32b119857437e
Luis Manuel Averhoff <laver008@fiu.edu>
2016-10-03 08:12:37 -0400

    Update matrix.c

--- data_structures/matrix/c/src/matrix.c
+++ data_structures/matrix/c/src/matrix.c
@@ -5,18 +5,10 @@
 
 int createMatrix(matrix *mat, const size_t rows, const size_t columns, const size_t memSize)
 {
-    void **data = malloc(sizeof(void *) * rows); // initialize the pointers with some pointers
-    data[0] = malloc(memSize * rows *  columns); // create a memory block that they can point to.
+    void *data = malloc(rows * columns * memSize);
 
-    if(data != NULL && data[0] != NULL)
+    if(data != NULL)
     {
-        size_t i;
-
-        for (i = 1; i < rows; i++)
-        {
-           data[i] = (char *)data[0] + (i * columns * memSize); // move each pointer to the first element in the ith row.
-        }
-
         mat->rows = rows;
         mat->columns = columns;
         mat->memSize = memSize;
@@ -34,13 +26,13 @@ void setElement(matrix *mat, const size_t x, const size_t y, const void *data)
        Remember this formula. (i * numOfColumns) + j to traverse a 2D array in a 1D fashion(This formula only applies to Row Major Order Arrays).
     */
     size_t offset = ((x * mat->columns) + y) * mat->memSize;
-    memcpy((char *)mat->data[0] + offset, data, mat->memSize);
+    memcpy((char *)mat->data + offset, data, mat->memSize);
 }
 
 void *getElement(matrix *mat, const size_t x, const size_t y)
 {
     size_t offset = ((x * mat->columns) + y) * mat->memSize;
-    return (char *)mat->data[0] + offset;
+    return (char *)mat->data + offset;
 }
 
 void freeMatrix(matrix *mat)
@@ -48,12 +40,10 @@ void freeMatrix(matrix *mat)
     if(mat->data != NULL)
     {
         /*Set back every back to their default values to preserve invariants.*/
-        free(mat->data[0]);
         free(mat->data);
         mat->data = NULL;
         mat->rows = 0;
         mat->columns = 0;
         mat->memSize = 0;
     }
-}
 }
